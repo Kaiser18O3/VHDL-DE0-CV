bb5f1a646b0b0bd9edeb6eaead253878206ead5b
Kaiser18O3 <stefan.kaiser@outlook.at>
2021-05-04 09:50:21 +0200

    Adds USRD for the Board

--- Board/Ultrasonic_rangefinder/USRD.vhd
+++ Board/Ultrasonic_rangefinder/USRD.vhd
@@ -7,7 +7,7 @@ entity USRD is
   port (
     clock_50 : in std_logic;
     echo     : in std_logic;
-    trigger  : out std_logic;
+    GPIO_0   : inout std_logic_vector(35 downto 0);
     HEX0     : out std_logic_vector(6 downto 0);
     HEX1     : out std_logic_vector(6 downto 0);
     HEX2     : out std_logic_vector(6 downto 0)
@@ -23,11 +23,13 @@ architecture rtl of USRD is
   signal h        : std_logic_vector(3 downto 0);
 begin
 
+GPIO_0(2) <= '1';
+
   USR : entity WORK.ULTRASONIC_RANGEFINDER
     port map(
       clock_50  => clock_50,
-      echo      => echo,
-      q_trigger => trigger,
+      echo      => GPIO_0(0),
+      q_trigger => GPIO_0(1),
       q_mm      => e,
       q_cm      => z,
       q_dm      => h
--- Entities/ULTRASONIC_RANGEFINDER.vhd
+++ Entities/ULTRASONIC_RANGEFINDER.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
-entity USRF is
+entity ULTRASONIC_RANGEFINDER is
   port (
     clock_50  : in std_logic;
     echo      : in std_logic;
@@ -15,54 +15,65 @@ entity USRF is
   );
 end entity;
 
-architecture rtl of USRF is
-  constant trigger_delay : integer := 10; -- delay between receiving the echo signal and sending the trigger signal (in us)
-  constant trigger_time  : integer := 5; -- duration of the trigger pulse (in us)
+architecture rtl of ULTRASONIC_RANGEFINDER is
+  constant measurement_cycle : integer := 600000; -- time one measurement cycle takes (in us)
+  constant trigger_time      : integer := 10; -- duration of the trigger pulse (in us)
 
   signal icnt_mm : std_logic_vector (3 downto 0) := x"0";
   signal icnt_cm : std_logic_vector (3 downto 0) := x"0";
   signal icnt_dm : std_logic_vector (3 downto 0) := x"0";
   signal nanos   : integer                       := 0;
   signal micros  : integer                       := 0;
+  signal enable  : std_logic                     := '0';
 begin
-  micros <= nanos/1000;
 
-  q_trigger <= '1' when micros >= trigger_delay and micros < (trigger_delay + trigger_time) else '0';
+  micros    <= nanos/1000;
+  q_trigger <= '1' when micros < trigger_time else '0';
+  
+    q_mm <= icnt_mm;
+      q_cm <= icnt_cm;
+      q_dm <= icnt_dm;
 
-  process (clock_50, echo)
+  process (clock_50)
   begin
     if clock_50'event and clock_50 = '1' then
-      if echo = '1' then
-        if micros >= trigger_delay then
-          q_mm <= icnt_mm;
-          q_cm <= icnt_cm;
-          q_dm <= icnt_dm;
-        end if;
-        nanos   <= 0;
-        icnt_mm <= x"0";
-        icnt_cm <= x"0";
-        icnt_dm <= x"0";
+      if micros >= measurement_cycle then
+        nanos <= 0;
       else
-        if micros >= trigger_delay then
-          if (nanos mod 580) = 0 then
-            if icnt_mm = x"9" then
-              icnt_mm <= x"0";
-              if icnt_cm = x"9" then
-                icnt_cm <= x"0";
-                if icnt_dm = x"9" then
-                  icnt_dm <= x"0";
-                else
-                  icnt_dm <= icnt_dm + 1;
-                end if;
-              else
-                icnt_cm <= icnt_cm + 1;
-              end if;
+        nanos <= nanos + 20;
+      end if;
+    end if;
+  end process;
+
+  process (nanos, echo)
+  begin
+    if echo'event and echo = '0' then
+     -- q_mm <= icnt_mm;
+     -- q_cm <= icnt_cm;
+      --q_dm <= icnt_dm;
+    end if;
+
+    if echo = '0' then
+      icnt_mm <= x"0";
+      icnt_cm <= x"0";
+      icnt_dm <= x"0";
+    else
+      if (nanos mod 680) = 0 then
+        if icnt_mm = x"9" then
+          icnt_mm <= x"0";
+          if icnt_cm = x"9" then
+            icnt_cm <= x"0";
+            if icnt_dm = x"9" then
+              icnt_dm <= x"0";
             else
-              icnt_mm <= icnt_mm + 1;
+              icnt_dm <= icnt_dm + 1;
             end if;
+          else
+            icnt_cm <= icnt_cm + 1;
           end if;
+        else
+          icnt_mm <= icnt_mm + 1;
         end if;
-        nanos <= nanos + 1;
       end if;
     end if;
 
